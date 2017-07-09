e54e9fb354f390ed1d2d7d630550c4704288a63c
Lucas Farias <extintion007@gmail.com>
2017-07-09 18:28:24 -0300

    primer approach
    primer approach

---
+++ materialize/js/home.js
@@ -0,0 +1,17 @@
+/*!
+ * Materialize v0.99.0 (http://materializecss.com)
+ * Copyright 2014-2017 Materialize
+ * MIT License (https://raw.githubusercontent.com/Dogfalo/materialize/master/LICENSE)
+ */
+ function this_is_a_link(){
+  console.log("Link clicked");
+ }
+
+$('#card-element').on('click',function(){
+    $(this).hide();
+});
+
+$('#TAB-1').on('click',function(){
+    console.log("First button clicked");
+    $('#card-element').show();
+})
