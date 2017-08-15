0a4f9fe17bd84334f9ba8e9d457412fea5e64dda
Your Name <you@example.com>
2017-08-15 18:26:16 -0300

    armado de recursos y roles

--- proyecto/default/app/controllers/codigoprovincial_controller.php
+++ proyecto/default/app/controllers/codigoprovincial_controller.php
@@ -1,30 +1,31 @@
 <?php
+
 /**
  * Carga del modelo Menus...
  */
 Load::models('codigoprovincial');
+
 class CodigoprovincialController extends AdminController {
+
     /**
      * Obtiene una lista para paginar las provincias
      */
-    public function index($page=1) 
-    {
+    public function index($page = 1) {
         $r = new Codigoprovincial();
         $this->listCodigoprovincial = $r->getCodigoprovincial($page);
     }
- 
+
     /**
      * Crea un Registro
      */
-    public function create ()
-    {
+    public function create() {
         /**
          * Se verifica si el usuario envio el form (submit) y si ademas 
          * dentro del array POST existe uno llamado "menus"
          * el cual aplica la autocarga de objeto para guardar los 
          * datos enviado por POST utilizando autocarga de objeto
          */
-        if(Input::hasPost('codigoprovincial')){
+        if (Input::hasPost('codigoprovincial')) {
             /**
              * se le pasa al modelo por constructor los datos del form y ActiveRecord recoge esos datos
              * y los asocia al campo correspondiente siempre y cuando se utilice la convención
@@ -32,31 +33,30 @@ class CodigoprovincialController extends AdminController {
              */
             $r = new Codigoprovincial(Input::post('codigoprovincial'));
             //En caso que falle la operación de guardar
-            if($r->create()){
+            if ($r->create()) {
                 Flash::valid('Operación exitosa');
                 //Eliminamos el POST, si no queremos que se vean en el form
                 Input::delete();
-                return;               
-            }else{
+                return;
+            } else {
                 Flash::error('Falló Operación');
             }
         }
     }
- 
+
     /**
      * Edita un Registro
      *
      * @param int $id (requerido)
      */
-    public function edit($id)
-    {
+    public function edit($id) {
         $r = new Codigoprovincial();
- 
+        
         //se verifica si se ha enviado el formulario (submit)
-        if(Input::hasPost('codigoprovincial')){
- 
-            if($r->update(Input::post('codigoprovincial'))){
-                 Flash::valid('Operación exitosa');
+        if (Input::hasPost('codigoprovincial')) {
+
+            if ($r->update(Input::post('codigoprovincial'))) {
+                Flash::valid('Operación exitosa');
                 //enrutando por defecto al index del controller
                 return Redirect::to();
             } else {
@@ -64,25 +64,25 @@ class CodigoprovincialController extends AdminController {
             }
         } else {
             //Aplicando la autocarga de objeto, para comenzar la edición
-            $this->codigoprovincial = $r->find_by_id((int)$id);
+            $this->codigoprovincial = $r->find_by_id((int) $id);
         }
     }
- 
+
     /**
      * Eliminar un menu
      * 
      * @param int $id (requerido)
      */
-    public function del($id)
-    {
+    public function del($id) {
         $r = new Codigoprovincial();
-        if ($r->delete((int)$id)) {
-                Flash::valid('Operación exitosa');
-        }else{
-                Flash::error('Falló Operación'); 
+        if ($r->delete((int) $id)) {
+            Flash::valid('Operación exitosa');
+        } else {
+            Flash::error('Falló Operación');
         }
- 
+
         //enrutando por defecto al index del controller
         return Redirect::to();
     }
+
 }
