36ea0137388976bf2296e4e6cf3a8da8add14ee9
Diego Gomez <diegocosas@gmail.com>
2017-08-22 20:37:51 -0300

    Agregado modelo tiporeporte

--- proyecto/default/app/models/tiporeporte.php
+++ proyecto/default/app/models/tiporeporte.php
@@ -11,7 +11,21 @@
          *
 
          */
+public function paginar($pagina = 1) {
+        return $this->paginate("page: $pagina");
+    }
 
+    /**
+     * Devuelve los usuarios de la bd Paginados.
+     * 
+     * @param  integer $pagina numero de pagina a mostrar
+     * @return array          resultado de la consulta
+     */
+    public function filtrar_por_nombre($tiporeporte, $pagina = 1) {
+        $cols = "tiporeporte.*";
+        $where = " nombretiporeporte ilike '%$tiporeporte%'";
+        return $this->paginate($where, "columns: $cols", "", "page: $pagina");
+    }
         public function getTiporeporte($page, $ppage=20)
 
         {
