e4ed48c398a25c4797c6a05acf0ca887a09a52f2
Your Name <you@example.com>
2017-08-25 18:06:47 -0300

    Template de registrar usuario ciudadano

---
+++ proyecto/default/public/js/registroUsuario.js
@@ -0,0 +1,35 @@
+$(function() {
+    $("#numero_documento").keypress(function(event) {
+        if (event.which != 8 && event.which != 0 && (event.which < 48 || event.which > 57)) {
+            $(".alert").html("Enter only digits!").show().fadeOut(2000);
+            return false;
+        }
+    });
+});
+
+$(function() {
+    $("#numero_telefono").keypress(function(event) {
+        if (event.which != 8 && event.which != 0 && (event.which < 48 || event.which > 57)) {
+            $(".alert").html("Enter only digits!").show().fadeOut(2000);
+            return false;
+        }
+    });
+});
+
+(function() {
+    $('form input').keyup(function() {
+
+        var empty = false;
+        $('form input').each(function() {
+            if ($(this).val() == '') {
+                empty = true;
+            }
+        });
+
+        if (empty) {
+            $('#submitButton').attr('disabled', 'disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        } else {
+            $('#submitButton').removeAttr('disabled'); // updated according to http://stackoverflow.com/questions/7637790/how-to-remove-disabled-attribute-with-jquery-ie
+        }
+    });
+})()
