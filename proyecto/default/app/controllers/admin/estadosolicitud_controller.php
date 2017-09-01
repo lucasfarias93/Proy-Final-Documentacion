c87c643bdea2ccf4f7b6e324dca00ecd2ed0970f
casa <casa@casa>
2017-08-31 23:46:10 -0300

    arreglos de validacion de fechas

--- proyecto/default/app/controllers/admin/estadosolicitud_controller.php
+++ proyecto/default/app/controllers/admin/estadosolicitud_controller.php
@@ -1,30 +1,31 @@
 <?php
+
 /**
  * Carga del modelo Menus...
  */
 Load::models('estadosolicitud');
+
 class EstadosolicitudController extends AdminController {
+
     /**
      * Obtiene una lista para paginar las provincias
      */
-    public function index($page=1) 
-    {
+    public function index($page = 1) {
         $es = new Estadosolicitud();
         $this->listEstadosolicitud = $es->getEstadosolicitud($page);
     }
- 
+
     /**
      * Crea un Registro
      */
-    public function crear ()
-    {
+    public function crear() {
         /**
          * Se verifica si el usuario envio el form (submit) y si ademas 
          * dentro del array POST existe uno llamado "menus"
          * el cual aplica la autocarga de objeto para guardar los 
          * datos enviado por POST utilizando autocarga de objeto
          */
-        if(Input::hasPost('estadosolicitud')){
+        if (Input::hasPost('estadosolicitud')) {
             /**
              * se le pasa al modelo por constructor los datos del form y ActiveRecord recoge esos datos
              * y los asocia al campo correspondiente siempre y cuando se utilice la convención
@@ -32,57 +33,63 @@ class EstadosolicitudController extends AdminController {
              */
             $r = new Estadosolicitud(Input::post('estadosolicitud'));
             //En caso que falle la operación de guardar
-            if($r->create()){
+            if ($r->create()) {
                 Flash::valid('Operación exitosa');
                 //Eliminamos el POST, si no queremos que se vean en el form
                 Input::delete();
-                return;               
-            }else{
+                return;
+            } else {
                 Flash::error('Falló Operación');
             }
         }
     }
- 
+
     /**
      * Edita un Registro
      *
      * @param int $id (requerido)
      */
-    public function edit($id)
-    {
+    public function edit($id) {
         $es = new Estadosolicitud();
- 
         //se verifica si se ha enviado el formulario (submit)
-        if(Input::hasPost('estadosolicitud')){
- 
-            if($es->update(Input::post('estadosolicitud'))){
-                 Flash::valid('Operación exitosa');
-                //enrutando por defecto al index del controller
-                return Redirect::to();
+        if (Input::hasPost('estadosolicitud')) {
+            $tr2 = Input::post('estadosolicitud');
+            $fechahasta = DateTime::createFromFormat("d/m/Y", $tr2["fechahasta"]);
+            $fechadesde = DateTime::createFromFormat("d/m/Y", $tr2["fechadesde"]);
+            if ($fechahasta && $fechadesde->format("d/m/Y") == $tr2["fechadesde"] && $fechadesde <= $fechahasta) {
+                if ($es->update(Input::post('estadosolicitud'))) {
+                    Flash::valid('Operación exitosa');
+                    //enrutando por defecto al index del controller
+                    return Redirect::to();
+                } else {
+                    Flash::error('Falló Operación');
+                }
             } else {
-                Flash::error('Falló Operación');
+                Flash::error('Falló fecha');
             }
         } else {
             //Aplicando la autocarga de objeto, para comenzar la edición
-            $this->estadosolicitud = $es->find_by_id((int)$id);
+            $this->estadosolicitud = $es->find_by_id((int) $id);
+            $this->estadosolicitud->fechahasta = UtilApp::formatea_fecha_bd_to_pantalla($this->estadosolicitud->fechahasta);
+            $this->estadosolicitud->fechadesde = UtilApp::formatea_fecha_bd_to_pantalla($this->estadosolicitud->fechadesde);
         }
     }
- 
+
     /**
      * Eliminar un menu
      * 
      * @param int $id (requerido)
      */
-    public function del($id)
-    {
+    public function del($id) {
         $es = new Estadosolicitud();
-        if ($es->delete((int)$id)) {
-                Flash::valid('Operación exitosa');
-        }else{
-                Flash::error('Falló Operación'); 
+        if ($es->delete((int) $id)) {
+            Flash::valid('Operación exitosa');
+        } else {
+            Flash::error('Falló Operación');
         }
- 
+
         //enrutando por defecto al index del controller
         return Redirect::to();
     }
+
 }
