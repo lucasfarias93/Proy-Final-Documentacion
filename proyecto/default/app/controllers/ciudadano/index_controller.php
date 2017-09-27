091a4bdde904398c58d824584b6dcc685c7ca84f
Diego Gomez <diegocosas@gmail.com>
2017-09-26 21:24:47 -0300

    Pruebas webservice

--- proyecto/default/app/controllers/ciudadano/index_controller.php
+++ proyecto/default/app/controllers/ciudadano/index_controller.php
@@ -9,12 +9,12 @@ class IndexController extends AdminController {
     }
 
     public function index() {
-       $servicio='http://10.160.1.229:8888/RCWebService.asmx/nacimiento_propia?'; //url del servicio
-//$parametros=array(); //parametros de la llamada
-//$parametros['dni']="29222236";
-//$client = new SoapClient($servicio, $parametros);
-//$result = $client->nacimiento_propia($parametros);//llamamos al métdo que nos interesa con los parámetros 
-//var_dump($result);
+//        $servicio = "http://10.160.1.229:8888/RCWebService.asmx/nacimiento_propia?wsdl"; //url del servicio
+//        $parametros = array(); //parametros de la llamada
+//        $parametros['dni'] = '34776423';
+//        $client = new SoapClient($servicio, $parametros);
+//        $result = $client->nacimiento_propia($parametros); //llamamos al métdo que nos interesa con los parámetros 
+//        var_dump($result);
     }
 
     public function buscar_parentesco_tipolibro() {
