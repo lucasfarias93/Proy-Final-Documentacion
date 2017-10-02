424f3e76a3fefe3626d709e646c04bdb14a32cfc
Your Name <you@example.com>
2017-10-02 17:18:15 -0300

    Visulaizacion de solicitudes

--- proyecto/default/app/models/solicitudacta.php
+++ proyecto/default/app/models/solicitudacta.php
@@ -20,9 +20,15 @@ class Solicitudacta extends ActiveRecord {
      * @return array          resultado de la consulta
      */
     public function buscar_solicitud_acta($id, $page, $ppage = 20) {
-        $cols = "solicitudacta.*";
+        $cols = "solicitudacta.*, p.nombreparentesco, c.codigodepago, t.nombretiposolicitud, se.fechacambioestado, es.nombreestadosolicitud";
         $where = " idusuario = '$id'";
-        return $this->paginate("page: $page", "per_page: $ppage", 'order: id desc');
+        $join = " join parentesco p on p.id = solicitudacta.idparentesco";
+                $join.=" join cupondepago c on c.id = solicitudacta.idcupondepago ";
+                $join.=" join tiposolicitudacta t on t.id = solicitudacta.idtiposolicitudacta ";
+                $join.=" join solicitudestado se on se.id = solicitudacta.ultimosolicitudestado";
+                $join.=" join estadosolicitud es on es.id = se.idestadosolicitud" ;
+        
+        return $this->find($where, "columns: $cols", "join: $join");
     }
 
     public function getSolicitudacta($page, $ppage = 20) {
