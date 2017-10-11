06d68ebfe295a5b73ca0b2da5a6e6d42a5314374
Diego Gomez <diegocosas@gmail.com>
2017-10-11 18:29:12 -0300

    Agregado las consultas de los tipos de reportes

--- proyecto/default/app/controllers/admin/reporte_controller.php
+++ proyecto/default/app/controllers/admin/reporte_controller.php
@@ -13,13 +13,25 @@ class ReporteController extends AdminController {
     public function index($page = 1) {
         $tr = new Tiporeporte();
         $this->listTiporeporte = $tr->getTiporeporte($page);
+        if (Input::hasPost('tiporeporte')) {
+            $tr = Input::post('tiporeporte');
+            foreach ($tr as $value) {
+                var_dump($value);
+                if ($value == 2) {
+                    var_dump("paso por el 2");
+                    $this->listTiporeporte = $tr->reporte_solicitudes_generadas();
+                    
+                }
+            }
+        }
     }
-    
+
     public function actasFirmadas() {
         
     }
-    
-    public function reportePDF(){
+
+    public function reportePDF() {
         
     }
+
 }
--- proyecto/default/app/models/tiporeporte.php
+++ proyecto/default/app/models/tiporeporte.php
@@ -25,10 +25,46 @@ class Tiporeporte extends ActiveRecord {
         return $this->paginate($where, "columns: $cols", "", "page: $pagina");
     }
     
-    public function reporte_actas_firmadas($tiporeporte, $pagina = 1) {
-        $cols = "tiporeporte.*";
-        $where = " nombretiporeporte ilike '%$tiporeporte%'";
-        return $this->paginate($where, "columns: $cols", "", "page: $pagina");
+    public function reporte_actas_firmadas($page, $ppage = 20) {
+        $cols = "solicitudacta.*, p.nombreparentesco, c.codigodepago, t.nombretiposolicitud, se.fechacambioestado, es.nombreestadosolicitud";
+        $where = " ultimosolicitudestado = '6'";
+        $join = " join parentesco p on p.id = solicitudacta.idparentesco";
+        $join .= " join cupondepago c on c.id = solicitudacta.idcupondepago ";
+        $join .= " join tiposolicitudacta t on t.id = solicitudacta.idtiposolicitudacta ";
+        $join .= " join solicitudestado se on se.id = solicitudacta.ultimosolicitudestado";
+        $join .= " join estadosolicitud es on es.id = se.idestadosolicitud";
+
+        return $this->find($where, "columns: $cols", "join: $join");
+    }
+        
+
+    public function reporte_solicitudes_generadas() {
+        $cols = "solicitudacta.*";
+        return $this->find("columns: $cols", "");
+    }
+
+    public function reporte_usuarios_generados() {
+        //falta hacer el COUNT
+        $cols = "usuarios.*";
+        return $this->find("columns: $cols", "");
+    }
+
+    public function reporte_reclamos_generados() {
+        //falta hacer el COUNT
+        $cols = "reclamos.*";
+        return $this->find("columns: $cols", "");
+    }
+
+    public function reporte_actas_por_vencer() {
+        //es dificil hacer despues
+        $cols = "reclamos.*";
+        return $this->find("columns: $cols", "");
+    }
+
+    public function reporte_ganancias() {
+        //para mas adelante
+        $cols = "reclamos.*";
+        return $this->find("columns: $cols", "");
     }
 
     public function getTiporeporte($page, $ppage = 20) {
