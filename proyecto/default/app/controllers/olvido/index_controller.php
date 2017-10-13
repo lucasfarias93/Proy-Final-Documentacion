7917856b074e6259bf220188296108956e944f8f
casa <casa@casa>
2017-10-13 01:20:43 -0300

    arreglos de mail

--- proyecto/default/app/controllers/olvido/index_controller.php
+++ proyecto/default/app/controllers/olvido/index_controller.php
@@ -9,91 +9,64 @@ class IndexController extends AppController {
 
         load::lib("phpmailer/class.phpmailer");
         load::lib("phpmailer/class.smtp");
-        $email_user = "diegocosas@gmail.com";
-        $email_password = "gringodiego";
-        $the_subject = "Phpmailer prueba by Evilnapsis.com";
-        $address_to = "dggomez@mendoza.gov.ar";
-        $from_name = "Evilnapsis";
-        $phpmailer = new PHPMailer();
-// ---------- datos de la cuenta de Gmail -------------------------------
-        $phpmailer->Username = $email_user;
-        $phpmailer->Password = $email_password;
-//-----------------------------------------------------------------------
-// $phpmailer->SMTPDebug = 1;
-        $phpmailer->SMTPSecure = 'ssl';
-        $phpmailer->Host = "smtp.gmail.com"; // GMail
-        $phpmailer->Port = 465;
-        $phpmailer->IsSMTP(); // use SMTP
-        $phpmailer->SMTPAuth = true;
-        $phpmailer->From =$from_name;
-        $phpmailer->AddAddress =$address_to; // recipients email
-        $phpmailer->Subject = $the_subject;
-        $phpmailer->Body .= "<h1 style='color:#3498db;'>Hola Mundo!</h1>";
-        $phpmailer->Body .= "<p>Mensaje personalizado</p>";
-        $phpmailer->Body .= "<p>Fecha y Hora: " . date("d-m-Y h:i:s") . "</p>";
-        $phpmailer->IsHTML(true);
-        $phpmailer->Send();
-
-
-
-//        view::template(NULL);
-//        try {
-//            if (Input::hasPost('usuarios')) {
-//                $usr = new Usuarios(Input::post('usuarios'));
-//                $usrbd = new Usuarios();
-//                $usrbd->filtrar_por_email($usr->email);
-//                if (!$usrbd) {
-//                    throw new NegocioExcepcion("El mail ingresado no existe");
-//                }
-//                ////Mandar mail
-//                load::lib("phpmailer/class.phpmailer");
-//                $mail = new PHPMailer();
-////Luego tenemos que iniciar la validación por SMTP:
-//                $mail->SMTPOptions = array(
-//                    'ssl' => array(
-//                        'verify_peer' => false,
-//                        'verify_peer_name' => false,
-//                        'allow_self_signed' => true
-//                    )
-//                );
-//                $mail->IsSMTP();
-//                $mail->SMTPDebug = 2;
-//                $mail->SMTPAuth = false;
-//                $mail->SMTPSecure = "ssl";
-//                $mail->Host = "smtp.gmail.com"; // SMTP a utilizar. Por ej. smtp.elserver.com
-//                $mail->Username = "diegocosas@gmail.com"; // Correo completo a utilizar
-//                $mail->Password = "gringodiego"; // Contraseña
-//                $mail->Port = 465; // Puerto a utilizar
-////Con estas pocas líneas iniciamos una conexión con el SMTP. Lo que ahora deberíamos hacer, es configurar el mensaje a enviar, el //From, etc.
-//                $mail->From = "diegocosas@gmail.com"; // Desde donde enviamos (Para mostrar)
-//                $mail->FromName = "Soporte";
-////Estas dos líneas, cumplirían la función de encabezado (En mail() usado de esta forma: “From: Nombre <correo@dominio.com>”) de //correo.
-//                $mail->AddAddress($usr->email); // Esta es la dirección a donde enviamos
-//                //$mail->AddAddress("dggomez@mendoza.gov.ar"); // Esta es la dirección a donde enviamos
-//                $mail->IsHTML(true); // El correo se envía como HTML
-//                $aux = $usrbd->id;
-//                $link = '<a href="http://190.15.213.87:81/recuperar/index/index/' . $aux . '">Aqui</a>';
-//                $mail->Subject = "Recuperacion de cuenta"; // Este es el titulo del email.
-//                $body = "Para recuperar tu cuenta hace click " . $link;
-//                $mail->Body = $body; // Mensaje a enviar
-//                $exito = $mail->Send(); // Envía el correo.
-////También podríamos agregar simples verificaciones para saber si se envió:
-//                if ($exito) {
-////                    $usrbd->clave = MyAuth::hash("");
-////                    $usrbd->update();
-//                    Flash::info("El correo fue enviado correctamente");
-//                    input::delete();
-//                    Router::redirect('login');
-//                } else {
-//                    $mail->ErrorInfo;
-//                    throw new NegocioExcepcion("No se pudo enviar el correo");
-//                    input::delete();
-//                }
-//            }
-//        } catch (NegocioExcepcion $e) {
-//            Logger::error($e->getMessage());
-//            Flash::info($e->getMessage());
-//        }
+        view::template(NULL);
+        try {
+            if (Input::hasPost('usuarios')) {
+                $usr = new Usuarios(Input::post('usuarios'));
+                $usrbd = new Usuarios();
+                $usrbd->filtrar_por_email($usr->email);
+                if (!$usrbd) {
+                    throw new NegocioExcepcion("El mail ingresado no existe");
+                }
+                ////Mandar mail
+                load::lib("phpmailer/class.phpmailer");
+                $mail = new PHPMailer();
+//Luego tenemos que iniciar la validación por SMTP:
+                $mail->SMTPOptions = array(
+                    'ssl' => array(
+                        'verify_peer' => false,
+                        'verify_peer_name' => false,
+                        'allow_self_signed' => true
+                    )
+                );
+                $mail->IsSMTP();
+                $mail->SMTPDebug = 2;
+                $mail->SMTPAuth = true;
+                $mail->SMTPSecure = 'ssl';
+                $mail->Host = "smtp.gmail.com"; // SMTP a utilizar. Por ej. smtp.elserver.com
+                $mail->Username = "diegocosas@gmail.com"; // Correo completo a utilizar
+                $mail->Password = "gringodiego"; // Contraseña
+                $mail->Port = 465; // Puerto a utilizar
+//Con estas pocas líneas iniciamos una conexión con el SMTP. Lo que ahora deberíamos hacer, es configurar el mensaje a enviar, el //From, etc.
+                $mail->From = "diegocosas@gmail.com"; // Desde donde enviamos (Para mostrar)
+                $mail->FromName = "Soporte";
+//Estas dos líneas, cumplirían la función de encabezado (En mail() usado de esta forma: “From: Nombre <correo@dominio.com>”) de //correo.
+                $mail->AddAddress($usr->email); // Esta es la dirección a donde enviamos
+                //$mail->AddAddress("dggomez@mendoza.gov.ar"); // Esta es la dirección a donde enviamos
+                $mail->IsHTML(true); // El correo se envía como HTML
+                $aux = $usrbd->id;
+                $link = '<a href="http://190.15.213.87:81/recuperar/index/index/' . $aux . '">Aqui</a>';
+                $mail->Subject = "Recuperacion de cuenta"; // Este es el titulo del email.
+                $body = "Para recuperar tu cuenta hace click " . $link;
+                $mail->Body = $body; // Mensaje a enviar
+                $exito = $mail->Send(); // Envía el correo.
+//También podríamos agregar simples verificaciones para saber si se envió:
+                if ($exito) {
+//                    $usrbd->clave = MyAuth::hash("");
+//                    $usrbd->update();
+                    Flash::info("El correo fue enviado correctamente");
+                    input::delete();
+                    Router::redirect('login');
+                } else {
+                    $mail->ErrorInfo;
+                    throw new NegocioExcepcion("No se pudo enviar el correo");
+                    input::delete();
+                }
+            }
+        } catch (NegocioExcepcion $e) {
+            Logger::error($e->getMessage());
+            Flash::info($e->getMessage());
+        }
     }
 
     public function correoandroid($email) {
