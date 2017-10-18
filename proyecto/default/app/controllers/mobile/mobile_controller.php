1fc6a4e01f2c92b9aa76735ac912ac930485afde
diego <diego@grinbook.home>
2017-10-18 14:04:02 -0300

    Arreglos template del backend

--- proyecto/default/app/controllers/mobile/mobile_controller.php
+++ proyecto/default/app/controllers/mobile/mobile_controller.php
@@ -10,31 +10,41 @@ class MobileController extends AppController {
 
     public function login($user, $pass) {
         view::select(null, null);
-        view::json(MyAuth::autenticar($user, $pass, TRUE));
-    }
-    public function verificar_validez_usuario_mobile($user, $pass, $id, $codigo) {
-        if(MyAuth::autenticar($user, $pass, TRUE)) {
-        view::select(NULL, NULL);
-        if ($id != NULL) {
-            $sola = new Solicitudacta();
-            $solacta = $sola->buscar_solicitud_acta_por_codigo_pago($id, $codigo, $page = 1);
-            $fecha = $solacta->fechacambioestado;
-            $fechaactual = UtilApp::fecha_actual();
-            $diasrestantes = 300 - UtilApp::calcular_dias_entre_fechas($fecha, $fechaactual);
-            if ($solacta != null) {
-                if ($diasrestantes > 1) {
-                    // Flash::info("Acta valida quedan " . $diasrestantes . " dias");
-                    view::json("Acta valida quedan " . $diasrestantes . " dias");
-                } else {
-                    Flash::error("Acta vencida");
-                }
+        if (MyAuth::autenticar($user, $pass, TRUE)) {
+            if (Auth::is_valid()) {
+                $this->redirect("ciudadano");
+                view::json(TRUE);
             } else {
-                Logger::info($solacta);
-                Flash::error("No existen actas con los datos ingresados");
+                view::json(FALSE);
             }
         } else {
-            Flash::error("debe ingresar un codigo de pago");
+            view::json(FALSE);
         }
+    }
+
+    public function verificar_validez_usuario_mobile($user, $pass, $id, $codigo) {
+        if (MyAuth::autenticar($user, $pass, TRUE)) {
+            view::select(NULL, NULL);
+            if ($id != NULL) {
+                $sola = new Solicitudacta();
+                $solacta = $sola->buscar_solicitud_acta_por_codigo_pago($id, $codigo, $page = 1);
+                $fecha = $solacta->fechacambioestado;
+                $fechaactual = UtilApp::fecha_actual();
+                $diasrestantes = 300 - UtilApp::calcular_dias_entre_fechas($fecha, $fechaactual);
+                if ($solacta != null) {
+                    if ($diasrestantes > 1) {
+// Flash::info("Acta valida quedan " . $diasrestantes . " dias");
+                        view::json("Acta valida quedan " . $diasrestantes . " dias");
+                    } else {
+                        Flash::error("Acta vencida");
+                    }
+                } else {
+                    Logger::info($solacta);
+                    Flash::error("No existen actas con los datos ingresados");
+                }
+            } else {
+                Flash::error("debe ingresar un codigo de pago");
+            }
         }
     }
 
