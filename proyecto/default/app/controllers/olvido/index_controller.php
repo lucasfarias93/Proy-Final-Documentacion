b022f3a48fd8464f3fe2b1154eb413c75eeb9303
Your Name <you@example.com>
2017-10-20 12:34:54 -0300

    Prueba mail

--- proyecto/default/app/controllers/olvido/index_controller.php
+++ proyecto/default/app/controllers/olvido/index_controller.php
@@ -19,7 +19,7 @@ class IndexController extends AppController {
                 }
                 ////Mandar mail
                 $mail = new PHPMailer();
-                $mail->SetLanguage("es", 'includes/phpMailer/language/');
+                $mail->SetLanguage("en", 'includes/phpMailer/language/');
 //Luego tenemos que iniciar la validación por SMTP:
                 $mail->IsSMTP();
                 $mail->SMTPDebug = 2;
