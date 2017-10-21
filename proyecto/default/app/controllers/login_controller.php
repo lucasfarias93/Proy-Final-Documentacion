aa298cfc92e2a1bbfb0c0f0f5eb9a8fd7298baa5
Gaston Grippi <gaston.grippi@gmail.com>
2017-10-21 18:38:56 -0300

    borrado de codigo comentado y agregado formato

--- proyecto/default/app/controllers/login_controller.php
+++ proyecto/default/app/controllers/login_controller.php
@@ -22,19 +22,6 @@ class LoginController extends AppController {
         view::select(NULL, 'logueo');
         //si viene un usuario logueado lo desologueo
         MyAuth::cerrar_sesion();
-//       echo "HOLA";
-//       Logger::info("Probando el servicio");
-//       view::select(NULL, NULL);
-//        view::select(null, NULL);
-//        $ret = $this->_logueoValido("diego", "00000");
-//        var_dump($ret);
-//
-//        if ($ret) {
-//            view::json(TRUE);
-//        } else {
-//            View::json(FALSE);
-//            var_dump($ret);
-//        }
     }
 
     public function verificar_validez_entidad() {
@@ -63,19 +50,6 @@ class LoginController extends AppController {
             Flash::error("debe ingresar un codigo de pago");
         }
     }
-
-//    public function get_login($user, $pass) {
-//        view::select(null, NULL);
-//        $ret = $this->_logueoValido("diego", "23000");
-//        var_dump($ret);
-//
-//        if ($ret) {
-//            view::json(TRUE);
-//        } else {
-//            View::json(FALSE);
-//            var_dump($ret);
-//        }
-//    }
 
     /**
      * Crea un Registro
