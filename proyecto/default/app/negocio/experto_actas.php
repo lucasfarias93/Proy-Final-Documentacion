5b35f82d3f5cad280d736f47a7742363a45c992d
Your Name <you@example.com>
2017-10-23 13:26:40 -0300

    Firma digital funcionando

--- proyecto/default/app/negocio/experto_actas.php
+++ proyecto/default/app/negocio/experto_actas.php
@@ -9,6 +9,7 @@
 class ExpertoActas {
 
     public static function generar_pdf($imagenes) {
+        $clave_key = 'registro';
         //verificamos que tenga permisos para imprimir
 //        echo $imagenes; 
 //        $imagenes = json_decode(stripslashes($imagenes));
@@ -49,8 +50,9 @@ class ExpertoActas {
 
         $nombre = 'pdf/acta_' . date("dmY", time()) . '.pdf';
         $pdf->Output($nombre, 'F');
-        $comando = "java -jar " . $_SERVER['DOCUMENT_ROOT'] . "jsignpdf/JSignPdf.jar " . $_SERVER['DOCUMENT_ROOT'] . "default/public" . $nombre . " -d " . $_SERVER['DOCUMENT_ROOT'] . PUBLIC_PATH . "default/public/pdf -kst BCPKCS12 -ksf /home/firma/certificado.p12 -ksp " . $clave_key . " --bg-path /home/firma/escudo.png --out-suffix '_firmado' --bg-scale 0.7 -fs 5 -a --l2-text 'Firmado Digitalmente por: \${signer} \${timestamp}' -urx 700 -ury 50 -lly 0 -llx 350 --page 1 -V";
-        //exec($comando);
+        ///// Comando para firmar digitalmente con jsignpdf
+        $comando = "java -jar " . $_SERVER['DOCUMENT_ROOT'] . "/proyecto/default/firma/jsignpdf/JSignPdf.jar " . $_SERVER['DOCUMENT_ROOT'] . "/proyecto/default/public/" . $nombre . " -d " . $_SERVER['DOCUMENT_ROOT'] . "/proyecto/default/public/pdf -kst BCPKCS12 -ksf " . $_SERVER['DOCUMENT_ROOT'] . "/proyecto/default/firma/certificado.p12 -ksp " . $clave_key . " --bg-path " . $_SERVER['DOCUMENT_ROOT'] . "/proyecto/default/firma/escudo.png --out-suffix '_firmado' --bg-scale 0.7 -fs 5 -a --l2-text 'Firmado Digitalmente por: \${signer} \${timestamp}' -urx 700 -ury 50 -lly 0 -llx 350 --page 1 -V";
+        exec($comando);
         $url = PUBLIC_PATH . $nombre;
         return $url;
     }
